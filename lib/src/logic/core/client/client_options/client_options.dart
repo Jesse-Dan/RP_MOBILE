51a997f6ef373bdb1641522fc70def7e0f02fdbe
Finn Jesse Dan <jessedan160@gmail.com>
2023-11-10 12:35:18 +0100

    set up csfr saving and add it to client option header, as well as creating global_constants

--- lib/src/logic/core/client/client_options/client_options.dart
+++ lib/src/logic/core/client/client_options/client_options.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+
 import 'package:recenth_posts/src/utils/constants/global_constants.dart';
 
 import '../../../services/storage_service/local_storage_service.dart';
@@ -7,28 +8,27 @@ class ClientOptions {
   static LocalStgService localStgService = LocalStgService();
 
   static BaseOptions getOptions(
-      {bool isAuth = false, bool isGetRequest = true}) {
-    return BaseOptions(
-      baseUrl: 'http://127.0.0.1:8000/rp_api/',
-      headers: isAuth
-          ? {
-              'Authorization': 'Bearer yourtokenhere',
-              'Content-Type': 'application/json',
-              GlobalConstants.CSRF_TOKEN:
-                  localStgService.getData(GlobalConstants.CSRF_TOKEN)
-            }
-          : {
-              'Content-Type': 'application/json',
-              GlobalConstants.CSRF_TOKEN:
-                  localStgService.getData(GlobalConstants.CSRF_TOKEN)
-            },
-      sendTimeout: isGetRequest ? null : const Duration(seconds: 30),
-      receiveTimeout: const Duration(seconds: 60),
-      contentType: Headers.jsonContentType,
-      responseType: ResponseType.json,
-      validateStatus: (status) {
-        return status! >= 200 && status < 400;
-      },
-    );
-  }
+          {bool isAuth = false, bool isGetRequest = true}) =>
+      BaseOptions(
+        baseUrl: 'http://127.0.0.1:8000/rp_api/',
+        headers: isAuth
+            ? {
+                'Authorization': 'Bearer yourtokenhere',
+                'Content-Type': 'application/json',
+                GlobalConstants.CSRF_TOKEN_KEY:
+                    localStgService.getData(GlobalConstants.CSRF_TOKEN)
+              }
+            : {
+                'Content-Type': 'application/json',
+                GlobalConstants.CSRF_TOKEN:
+                    localStgService.getData(GlobalConstants.CSRF_TOKEN)
+              },
+        sendTimeout: isGetRequest ? null : const Duration(seconds: 30),
+        receiveTimeout: const Duration(seconds: 60),
+        contentType: Headers.jsonContentType,
+        responseType: ResponseType.json,
+        validateStatus: (status) {
+          return status! >= 200 && status < 400;
+        },
+      );
 }
--- lib/src/utils/constants/global_constants.dart
+++ lib/src/utils/constants/global_constants.dart
@@ -1,5 +1,3 @@
-
-
 // ignore_for_file: constant_identifier_names
 
 class GlobalConstants {
@@ -8,7 +6,7 @@ class GlobalConstants {
   static const kMidiText = 14.0;
   static const kZeroText = 0.0;
 
-
   /// STORAGE KEYS
-  static const String CSRF_TOKEN ='CSRF-TOKEN';
+  static const String CSRF_TOKEN = 'CSRF-TOKEN';
+  static const String CSRF_TOKEN_KEY = 'X-CSRF-TOKEN';
 }
